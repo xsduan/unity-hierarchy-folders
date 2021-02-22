089ddc221fc30820a0e94de3885b4d912c96384c
SolidAlloy <perepelartem@gmail.com>
2021-02-22 18:47:13 +0200

    Started generating keywords from field names
    Since more fields are to be added to the settings, it becomes tedious to update the keywords each time. This approach will generate keywords on fly.

--- Editor/Settings/SettingsDrawer.cs
+++ Editor/Settings/SettingsDrawer.cs
@@ -8,32 +8,63 @@
 
     internal static class SettingsDrawer
     {
-        private static readonly GUIContent _buildStrippingName = new GUIContent("Build Stripping Type");
+        /// <summary>
+        /// Field names of corresponding settings. Each field name can be accessed by the name of the setting variable.
+        /// </summary>
+        private static readonly Dictionary<string, string> _fieldNames = new Dictionary<string, string>
+        {
+            { nameof(StripSettings.PlayMode), "Play Mode Stripping Type" },
+            { nameof(StripSettings.Build), "Build Stripping Type" },
+            { nameof(StripSettings.CapitalizeName), "Capitalize Folder Names" }
+        };
+
+        private static readonly GUIContent _buildStrippingName = new GUIContent(_fieldNames[nameof(StripSettings.Build)]);
 
         [SettingsProvider]
         public static SettingsProvider CreateSettingsProvider()
         {
             var provider = new SettingsProvider("Preferences/Hierarchy Folders", SettingsScope.User)
             {
-                guiHandler = searchContext =>
-                {
-                    StripSettings.PlayMode = (StrippingMode) EditorGUILayout.EnumPopup(
-                        "Play Mode Stripping Type", StripSettings.PlayMode);
-
-                    if (StripSettings.PlayMode == StrippingMode.ReplaceWithSeparator)
-                    {
-                        StripSettings.CapitalizeName =
-                            EditorGUILayout.Toggle("Capitalize Folder Names", StripSettings.CapitalizeName);
-                    }
-
-                    StripSettings.Build = (StrippingMode) EditorGUILayout.EnumPopup(
-                        _buildStrippingName, StripSettings.Build, TypeCanBeInBuild, true);
-                },
-
-                keywords = new HashSet<string>(new[] { "Play", "Mode", "Build", "Stripping", "Type", "Capitalize", "Folder", "Names" })
+                guiHandler = OnGUI,
+                keywords = GetKeywords()
             };
 
             return provider;
+        }
+
+        private static void OnGUI(string searchContext)
+        {
+            StripSettings.PlayMode = (StrippingMode) EditorGUILayout.EnumPopup(
+                _fieldNames[nameof(StripSettings.PlayMode)], StripSettings.PlayMode);
+
+            if (StripSettings.PlayMode == StrippingMode.ReplaceWithSeparator)
+            {
+                StripSettings.CapitalizeName = EditorGUILayout.Toggle(
+                    _fieldNames[nameof(StripSettings.CapitalizeName)], StripSettings.CapitalizeName);
+            }
+
+            StripSettings.Build = (StrippingMode) EditorGUILayout.EnumPopup(
+                _buildStrippingName, StripSettings.Build, TypeCanBeInBuild, true);
+        }
+
+        private static HashSet<string> GetKeywords()
+        {
+            var keywords = new HashSet<string>();
+
+            foreach (string fieldName in _fieldNames.Values)
+            {
+                keywords.AddWords(fieldName);
+            }
+
+            return keywords;
+        }
+
+        private static void AddWords(this HashSet<string> set, string phrase)
+        {
+            foreach (string word in phrase.Split(' '))
+            {
+                set.Add(word);
+            }
         }
 
         private static bool TypeCanBeInBuild(Enum enumValue)
