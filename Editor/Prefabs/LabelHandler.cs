b11e7a7c327d0a2b4dfe86d7ee8758e471e8ff73
SolidAlloy <perepelartem@gmail.com>
2021-02-22 17:02:53 +0200

    Moved label-adding logic from OnWillCreateAsset to OnPostprocessAllAssets and added label-removing logic
    OnPostprocessAllAssets is more convenient in that the assets can already be loaded from AssetDatabase without a need for coroutines, and they support more cases (like when changes are applied to prefab from its override)

---
+++ Editor/Prefabs/LabelHandler.cs
@@ -0,0 +1,70 @@
+﻿namespace UnityHierarchyFolders.Editor.Prefabs
+{
+    using System.Linq;
+    using Runtime;
+    using UnityEditor;
+    using UnityEngine;
+    using UnityEngine.Assertions;
+
+    public class LabelHandler : AssetPostprocessor
+    {
+        public const string FolderPrefabLabel = "FolderUser";
+
+        private static void OnPostprocessAllAssets(string[] importedAssets, string[] _, string[] __, string[] ___)
+        {
+            try
+            {
+                // Group imports into one to improve performance in case there are multiple prefabs that need a label change.
+                AssetDatabase.StartAssetEditing();
+
+                foreach (string assetPath in importedAssets)
+                {
+                    if (assetPath.EndsWith(".prefab"))
+                        HandlePrefabLabels(assetPath);
+                }
+            }
+            finally
+            {
+                AssetDatabase.StopAssetEditing();
+            }
+        }
+
+        private static void HandlePrefabLabels(string assetPath)
+        {
+            var asset = AssetDatabase.LoadAssetAtPath<GameObject>(assetPath);
+
+            if (asset.GetComponentsInChildren<Folder>().Length == 0)
+            {
+                RemoveFolderLabel(asset, assetPath);
+            }
+            else
+            {
+                AddFolderLabel(asset, assetPath);
+            }
+        }
+
+        private static void RemoveFolderLabel(GameObject assetObject, string assetPath)
+        {
+            var labels = AssetDatabase.GetLabels(assetObject);
+
+            if ( ! labels.Contains(FolderPrefabLabel))
+                return;
+
+            ArrayUtility.Remove(ref labels, FolderPrefabLabel);
+            AssetDatabase.SetLabels(assetObject, labels);
+            AssetDatabase.ImportAsset(assetPath);
+        }
+
+        private static void AddFolderLabel(GameObject assetObject, string assetPath)
+        {
+            var labels = AssetDatabase.GetLabels(assetObject);
+
+            if (labels.Contains(FolderPrefabLabel))
+                return;
+
+            ArrayUtility.Add(ref labels, FolderPrefabLabel);
+            AssetDatabase.SetLabels(assetObject, labels);
+            AssetDatabase.ImportAsset(assetPath);
+        }
+    }
+}
